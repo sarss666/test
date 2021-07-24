dc0ac7701a939f5368a3a4884cd53ff354f0ad73
sars666 <1491243047@qq.com>
2021-07-24 11:35:20 +0800

    代码

---
+++ 20210703/test.c
@@ -0,0 +1,92 @@
+
+#include <stdio.h>
+#pragma warning(disable:4996)
+#define AGE 80//宏,见名知意，提升代码的可维护性
+
+
+
+int main()
+{ 
+	
+   
+}
+//'x'括起来的叫字符，只能写一个字符；
+//"hssa"称为字符串
+//const int x = 100;//初始化,const变量不能修改
+	//x = 200;//赋值
+
+	//AGE = 10;enum//枚举
+
+	/*printf("%d\n", AGE);
+	int x = AGE;
+	printf("%d\n", x);*/
+	//int x = 0;
+	//int y = 0;
+	//printf("请输入对应数据");
+	//scanf("%d %d", &x,&y);
+	//int z = x + y;
+	//printf("result: %d\n", z);
+	//return 0;
+//int global = 100;
+//int main()
+//{
+//	int x = 200;
+//	int global = 300;//就近原则输出300.
+//	printf("%d, %d", x, global);
+//	return 0;
+//}
+////全局变量
+//double weight = 160.5;
+//void fun()
+//{
+//	printf("%f\n", weight);
+//	//printf("%f\n", high);
+//
+//}
+//int main()
+//{
+//  //局部变量
+//	double high = 190.8;
+//	printf("%f\n", weight);
+//	printf("%f\n", high);
+//	fun();
+//	return 0;
+//}
+	/*int high = 190;
+	float money = 1.8f;
+	char phone[12] = "12345678901";*/	
+	/*double m1 = 10.5;
+	double m2 = 1.5;
+	printf("%f\n", m1 + m2);*/
+/*double pi = 3.14;
+	double r = 2;
+	printf("面积： %f\n", pi * r * r);
+
+	return 0;*/
+//int main() 
+//{
+//    char  c = 'A';
+//    for (; c <= 'Z'; c++) {
+//        printf("%c ", c);
+//    }
+//    printf("\n");
+//    return 0;
+//}
+//int main()
+//{
+//	printf("char:  %d\n", sizeof(char));//1
+//	printf("short: %d\n", sizeof(short));//2
+//	printf("int: %d\n", sizeof(int));//4
+//	printf("float: %d\n", sizeof(float));//4
+//	printf("long: %d\n", sizeof(long));//4
+//	printf("llong: %d\n", sizeof(long long));//8
+//	printf("double: %d\n", sizeof(double));//8
+//	printf("long double: %d\n", sizeof(long double));//8
+//	return 0;
+//}
+//int main()
+//{
+//	printf("hello bite!\n");
+//
+//	return 0;
+// }
---
+++ 20270705/test1.c
@@ -0,0 +1,43 @@
+//常量，指程序运行过程中，值不能发生改变
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+	
+	printf("%d \n",strlen("abcde"));
+	
+	return 0;
+
+}
+
+
+
+ 
+
+//字符:各种字母,阿拉伯数字,各种标点符号.使用数字来表示字符ASCII码表
+//字符串 : 把一串字符放在一起.
+//\n，换行符。带反斜杠的叫转义字符
+//
+
+//#define A 10
+//
+//int main()
+//
+//{
+//	printf("a= %d\n", A);
+//	return 0;
+//}	
+
+//enum sex {
+//	MALE,
+//	FEMALE,
+//	UNKNOWN,
+//};
+/*.....................const int a = 10;//a的修改在运行过程中；
+	//const表示接下来的常量不能修改
+	printf("a=%d\n", a);
+	a = 20;
+	printf("a= %d\n", a);
+	return 0;.........................*/
+    
